e46275d62b162e95f00d419c2876f9b96e480475
Igor Almeida <igor.contato@gmail.com>
2010-06-30 23:35:44 +0200

    Use testing suite

--- readline_test.go
+++ readline_test.go
@@ -1,19 +1,19 @@
 // test program for the readline package
-package main
+package readline
 
-import "readline"
+import "testing"
 
-func main() {
+func TestReadline(t *testing.T) {
 	prompt := "by your command> ";
 
 	//loop until ReadLine returns nil (signalling EOF)
 	L: for {
-		switch result := readline.ReadLine(&prompt); true {
+		switch result := ReadLine(&prompt); true {
 		case result == nil: break L //exit loop
 
 		case *result != "": //ignore blank lines
 			println(*result);
-			readline.AddHistory(*result); //allow user to recall this line
+			AddHistory(*result); //allow user to recall this line
 		}
 	}
 }
